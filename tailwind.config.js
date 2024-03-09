2b64f0af5548a23b9e9cbae6126d508a6b994911
OwenLobato <owenlobatov@gmail.com>
2024-03-09 15:18:22 -0600

    Add colors theme vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgPrimary: 'var(--color-bg-primary)',
+        bgSecondary: 'var(--color-bg-secondary)',
+        textTilte: 'var(--color-text-title)',
+        textSubTitle: 'var(--color-text-subtitle)',
+        textContent: 'var(--color-text-content)',
+        btnHover: 'var(--color-hover)',
+        btn: 'var(--color-btn)',
+      },
+    },
   },
   plugins: [],
 };
