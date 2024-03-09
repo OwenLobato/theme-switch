d1884c5ea3c3d6ddcb72db42aabff93988e0b40e
OwenLobato <owenlobatov@gmail.com>
2024-03-09 15:19:05 -0600

    Use context to set theme on principal container

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,32 @@ import { Content } from './components/Content';
 import { ThemeSwitch } from './components/ThemeSwitch';
 import { Card } from './components/Card';
 import { Form } from './components/Form';
-import { ThemeContext } from './contexts/ThemeContext';
+import { useThemeContext } from './contexts/ThemeContext';
 
 function App() {
+  const { theme } = useThemeContext();
+
   return (
-    <ThemeContext>
-      <div className='min-h-screen bg-blue-200 flex flex-col'>
-        <Header />
-        <ThemeSwitch />
-        <Content />
-        <div className='flex justify-center'>
-          <Card
-            title='Title 1'
-            description='Description 1'
-            image='https://via.placeholder.com/150'
-          />
-          <Card
-            title='Title 2'
-            description='Description 2'
-            image='https://via.placeholder.com/150'
-          />
-          <Form />
-        </div>
+    <div
+      className={`theme-${theme} min-h-screen bg-bgPrimary text-textContent flex flex-col`}
+    >
+      <Header />
+      <ThemeSwitch />
+      <Content />
+      <div className='flex justify-center'>
+        <Card
+          title='Title 1'
+          description='Description 1'
+          image='https://via.placeholder.com/150'
+        />
+        <Card
+          title='Title 2'
+          description='Description 2'
+          image='https://via.placeholder.com/150'
+        />
+        <Form />
       </div>
-    </ThemeContext>
+    </div>
   );
 }
 
